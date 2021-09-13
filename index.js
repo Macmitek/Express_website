f29692e0e484f10ac186fd20bd3bc291e255e304
Mitesh Jadav <jadavmitesh.jd@gmail.com>
2021-09-13 18:57:27 +0530

    sample routes

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-import express from 'express';
+import express, { response } from 'express';
 import data from './data/data.json';
 
 const app = express();
 const PORT = 3000;
+
+app.get('/', (req, res) => {
+  //get data first
+  res.json(data);
+});
+
+app.post('/newItem', (req, res) => {
+  res.send(`A post request with /newItem on port ${PORT}`);
+});
+
+app.put('/item', (req, res) => {
+  res.send(`A put request with /item route on port ${PORT}`);
+});
+
+app.delete('/item', (req, res) => {
+  res.send(`A get request with / route on port ${PORT}`);
+});
 
 app.listen(PORT, () => {
   console.log(`Your server is running on the port ${PORT}`);
