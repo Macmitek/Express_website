c37f06890ffc576125b332129d1435c5053ec418
Mitesh Jadav <jadavmitesh.jd@gmail.com>
2021-09-14 13:59:38 +0530

    favicon added

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import favicon from 'serve-favicon';
+import path from 'path';
 import data from './data/data.json';
 
 const app = express();
@@ -6,16 +8,24 @@ const PORT = 3000;
 
 // This is for the public folder on path /
 app.use(express.static('public'));
-//method to use json
+
+// method to use JSON
 //app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 // this is for images folder on path images
 app.use('/images', express.static('images'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.get('/', (req, res) =>
   // get data first
   res.json(data)
 );
+
+// JSON data
+// { "hello": "JSON is cool"}
+// URLEncoded data
+// hello=
 
 app.post('/newItem', (req, res) => {
   console.log(req.body);
@@ -43,10 +53,11 @@ app.get(
 app
   .route('/item')
   .get((req, res) => {
+    throw new Error();
     //res.download('images/rocket.jpg')
     //res.redirect('http://www.linkedin.com')
     //res.end()
-    res.send(`a get request with /item route on port ${PORT}`);
+    //res.send(`a get request with /item route on port ${PORT}`)
   })
   .put((req, res) =>
     res.send(`a put request with /newItem route on port ${PORT}`)
@@ -54,6 +65,12 @@ app
   .delete((req, res) =>
     res.send(`a delete request with /item route on port ${PORT}`)
   );
+
+// Error handling function
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send(`Red alert! Red alert!: ${err.stack}`);
+});
 
 app.listen(PORT, () => {
   console.log(`Your server is running on port ${PORT}`);
